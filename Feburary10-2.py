7d37a8ad62ea9d60451e792271ff120afde6be75
Umair-Khalid-101 <umairkhalid.github@gmail.com>
2023-02-11 00:52:50 +0500

    Feb-11

--- Feburary10-2.py
+++ Feburary10-2.py
@@ -1,12 +1,15 @@
 # Count Odd Numbers in an Interval Range
 # Given two non-negative integers low and high. Return the count of odd numbers between low and high (inclusive).
 
-def numberOfOdd(a,b):
-  if(b%2 == 1):
-    return (b-a)//2 + 1
-  elif(b%2 == 0):
-    return (b-a)//2
+import math
+
+def numberOfOdd(low,high):
+  if(high%2 == 1):
+    return (high-low)//2 + 1
+  elif(high%2 == 0):
+    return int(math.ceil((high-low)/2))
 
 print("No of Odd Numbers in Range(3,7)",numberOfOdd(3,7))
 print("No of Odd Numbers in Range(8,10)",numberOfOdd(8,10))
 print("No of Odd Numbers in Range(4,9)",numberOfOdd(4,9))
+print("No of Odd Numbers in Range(21,22)",numberOfOdd(21,22))
