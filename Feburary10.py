7068ff1b970b39dab95e4231c49c2b1f5a512d2e
Umair-Khalid-101 <umairkhalid.github@gmail.com>
2023-02-10 22:58:05 +0500

    Feb-10

---
+++ Feburary10.py
@@ -0,0 +1,19 @@
+# Longest Substring Without Repeating Characters
+# The task is to find the longest substring in a given string that does not contain any repeating characters.
+
+def lengthOfLongestSubstring(s: str) -> int:
+    char_set = set()
+    max_length = 0
+    i = 0
+    j = 0
+    while (j < len(s)):
+        if s[j] not in char_set:
+            char_set.add(s[j])
+            j += 1
+            max_length = max(max_length, j - i)
+        else:
+            char_set.remove(s[i])
+            i += 1
+    return max_length
+
+print("Length of longest Substring is:",lengthOfLongestSubstring("pwwkew"))
