d04998ce01a84e5907737c57a970df1c7eabfb86
Umair-Khalid-101 <umairkhalid.github@gmail.com>
2023-02-10 23:46:09 +0500

    Feb-10

---
+++ Feburary10-3.py
@@ -0,0 +1,8 @@
+# Count Odd Numbers in an Interval Range
+# Given two non-negative integers low and high. Return the count of odd numbers between low and high (inclusive).
+
+def numberOfOdd(a,b):
+  print(a)
+  print(b)
+
+print(numberOfOdd(3,7))
