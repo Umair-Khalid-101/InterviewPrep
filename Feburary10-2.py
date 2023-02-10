da90068da05a280085313875ace6bedd54716b59
Umair-Khalid-101 <umairkhalid.github@gmail.com>
2023-02-11 00:26:08 +0500

    Feb-10

--- Feburary10-2.py
+++ Feburary10-2.py
@@ -2,7 +2,11 @@
 # Given two non-negative integers low and high. Return the count of odd numbers between low and high (inclusive).
 
 def numberOfOdd(a,b):
-  print(a)
-  print(b)
+  if(b%2 == 1):
+    return (b-a)//2 + 1
+  elif(b%2 == 0):
+    return (b-a)//2
 
-print(numberOfOdd(3,7))
+print("No of Odd Numbers in Range(3,7)",numberOfOdd(3,7))
+print("No of Odd Numbers in Range(8,10)",numberOfOdd(8,10))
+print("No of Odd Numbers in Range(4,9)",numberOfOdd(4,9))
