f345ec28a9bb0d01a5234c333e1964ad37b98379
Umair-Khalid-101 <umairkhalid.github@gmail.com>
2023-02-12 23:56:33 +0500

    Feb-12

--- Feburary-12.py
+++ Feburary-12.py
@@ -1,11 +1,11 @@
 # Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 # You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
-nums = [2,7,11,15]
-target = 9
-
-def twoSum(self, nums, target):
+def my_function(nums,target):
+  print("Hello from a function")
   print(nums)
   print(target)
 
-  twoSum(nums,target)
+nums = [2,3,4]
+target = 6
+my_function(nums,target)
